0e4f4797fa08dba8ba0802728d2cfca7d7ace7b3
Scott Garner <scott@j38.net>
2014-05-02 10:32:12 -0400

    Style and video changes.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var popcorn;
 
+$(document).ready( function() {
+
+	$("#watchVideo").click( function() {
+		$("#coverImage").fadeOut("slow", function() {
+			popcorn.play(0);
+		});
+	});
+
+	popcorn = Popcorn("#videoTrailer");
+
+	popcorn.on("playing", function() {
+		$("#ipadVideo").show();
+	});
+	popcorn.on("ended", function() {
+		
+		$("#ipadVideo").fadeOut("slow", function() {
+			$("#coverImage").fadeIn();
+		});
+
+	});
+
+});
